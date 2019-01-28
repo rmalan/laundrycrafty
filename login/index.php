75f88828a748d418d114814f39a89e2e61eb750e
RM Alan <m.rakamaulanaalan@studentpartner.com>
2019-01-28 22:57:42 +0700

    update login system

--- login/index.php
+++ login/index.php
@@ -1,52 +1,68 @@
 <?php
-   session_start();
-   if(isset($_SESSION['id_user'])) {
-   header('location:index.php'); }
-   require_once("../koneksi.php");
+    session_start();
+
+    require '../functions.php';
+
+    if(isset($_SESSION["login"])) {
+        header("Location: ../index.php");
+        exit;
+    }
+
+    if(isset($_POST["login"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+
+        if(mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+            if(password_verify($password, $row["password"])) {
+                $_SESSION["login"] = true;
+                $_SESSION["user"] = $row["username"];
+                header("Location: ../index.php");
+                exit;
+            }
+        }
+
+        $error = true;
+    }    
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="utf-8" />   
-    <meta name="viewport" content="width=device-width, initial-scale=1" />    
-    <title>Login | Laundry Crafty</title>    
-    <link href="/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="/css/signin.css" rel="stylesheet" />
-    <script src="/js/jquery-1.12.3.min.js"></script>
-    <script src="/js/bootstrap.js"></script>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+        <meta name="generator" content="Jekyll v3.8.5">
+        <title>Login | Laundry Crafty</title>
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <link rel="stylesheet" href="signin.css">    
+    </head>
+    <body class="text-center">
+        <form class="form-signin" action="" method="post">
+            <img class="mb-4" src="icons8-the-flash-sign.svg" alt="" width="86" height="86">
 
-<body background="../img/page-background.png">
-    <div class="container">
-        <form class="form-signin" action="../login/proseslogin.php" method="post">
-            <center>
-                <h2 class="form-signin-heading">
-                    <span class="glyphicon glyphicon-th-large"></span>Laundry Crafty
-                </h2>
-            </center>
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="glyphicon glyphicon-user"></i>
-                </span>
-                <input type="text" id="username" name="username" class="form-control" placeholder="Username" autocomplete="off" autofocus="on" required />
+            <h1 class="h3 mb-3 font-weight-normal">Laundry Crafty</h1>
+
+            <label for="username" class="sr-only">Username</label>
+            <input type="text" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
+
+            <label for="password" class="sr-only">Password</label>
+            <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+
+            <div class="checkbox mb-3">
+                <label>
+                <input type="checkbox" value="remember-me"> Remember Me
+                </label>
             </div>
-            <div class="input-group" style="margin-top: 5px;">
-                <span class="input-group-addon">
-                    <i class="glyphicon glyphicon-lock"></i>
-                </span>
-                <input type="password" id="password" name="password" class="form-control" placeholder="Password" autocomplete="off" required />
-            </div>
-            <br />
-            <button class="btn btn-lg btn-primary btn-block" value="Login" type="submit">Sign in</button>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Login</button>
+            <?php if(isset($error)) : ?>
+                <script>alert('Username atau password salah!')</script>
+            <?php endif; ?>
+            <p class="mt-5 mb-3 text-muted">&copy; 2019</p>
         </form>
-
-    </div><!-- /container -->
-
-    <center>
-        <h5 class="form-signin">
-            Copyright &copy; 2017
-        </h5>
-    </center>
-</body>
+    </body>
 </html>
--- login/logout.php
+++ login/logout.php
@@ -1,5 +1,8 @@
 <?php
-   session_start();
-   session_destroy();
-   header('location:../index.php');
+    session_start();
+    session_unset();
+    session_destroy();
+
+    header("Location: index.php");
+    exit;
 ?>
